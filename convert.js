0076af43b1ab5a040f11bfd5b617ce79c8c9c889
okmttdhr <10101114fin@gmail.com>
2018-01-17 17:50:40 +0900

    log standardDeviation

--- convert.js
+++ convert.js
@@ -2,8 +2,10 @@ const csv = require('csv');
 const fs = require('fs');
 const path = require('path');
 
-const FILE = path.join(__dirname, '../../Downloads/train.csv');
+// const FILE = path.join(__dirname, '../../Downloads/ml/test.csv');
+const FILE = path.join(__dirname, '../../Downloads/ml/train.csv');
 const wstream = fs.createWriteStream('./temp.csv');
+// const wstream = fs.createWriteStream('./temp2.csv');
 
 const sum = (arr) => {
   return arr.reduce((prev, current, i, arr) => {
@@ -15,8 +17,28 @@ const average = (arr) => {
   return sum(arr) / arr.length;
 };
 
+const variance = (arr) => {
+  var ave = average(arr);
+  var varia = 0;
+  for (i = 0; i < arr.length; i++) {
+    varia = varia + Math.pow(arr[i] - ave, 2);
+  }
+  return (varia / arr.length);
+}
+
+const standardDeviation = (arr) => {
+  var varia = variance(arr);
+  return Math.sqrt(varia);
+}
+
 const parser = csv.parse({trim: true}, (err, data) => {
   const rows = [];
+
+  const sibsp = [];
+  const parch = [];
+  const fare = [];
+  const age = [];
+
   data.forEach((columns, index) => {
     if (index === 0 || index === 1) {
       console.log('columns', columns);
@@ -37,11 +59,33 @@ const parser = csv.parse({trim: true}, (err, data) => {
     // age
     console.log('columns[5]: before', columns[5]);
     const averageAge = 23.79929292929293;
-    // columns[5] = Number(columns[5]);
+    const standardDeviationAge = 13.206201002454545;
     if (!columns[5]) {
       columns[5] = averageAge;
-      console.log('columns[5]: after', columns[5]);
     }
+    columns[5] = (columns[5] - averageAge) / standardDeviationAge;
+    console.log('columns[5]: after', columns[5]);
+
+    // sibsp
+    console.log('columns[6]: before', columns[6]);
+    const averageSibsp = 0.5230078563411896;
+    const standardDeviationSibsp = 1.1021244350892836;
+    columns[6] = (columns[6] - averageSibsp) / standardDeviationSibsp;
+    console.log('columns[6]: after', columns[6]);
+
+    // parch
+    console.log('columns[7]: before', columns[7]);
+    const averageParch = 0.38159371492704824;
+    const standardDeviationParch = 0.8056047612452284;
+    columns[7] = (columns[7] - averageParch) / standardDeviationParch;
+    console.log('columns[7]: after', columns[7]);
+
+    // fare
+    console.log('columns[9]: before', columns[9]);
+    const averageFare = 32.2042079685746;
+    const standardDeviationFare = 49.66553444477411;
+    columns[9] = (columns[9] - averageFare) / standardDeviationFare;
+    console.log('columns[9]: after', columns[9]);
 
     // embarked
     console.log('columns[11]: before', columns[11]);
@@ -73,20 +117,31 @@ const parser = csv.parse({trim: true}, (err, data) => {
         return Number(c);
       })
       ;
-    // }).join(',');
 
     // trainX
     rows.push(row);
+
+    if (index !== 0) {
+      sibsp.push(Number(columns[6]));
+      parch.push(Number(columns[7]));
+      fare.push(Number(columns[9]));
+      age.push(Number(columns[5]));
+    }
 
     // trainY
     // rows.push(columns[1]);
   });
 
-  // const csvContent = rows.join('\n');
+  console.log('average(sibsp)', average(sibsp));
+  console.log('standardDeviation(sibsp)', standardDeviation(sibsp));
+  console.log('average(parch)', average(parch));
+  console.log('standardDeviation(parch)', standardDeviation(parch));
+  console.log('average(fare)', average(fare));
+  console.log('standardDeviation(fare)', standardDeviation(fare));
+  console.log('average(age)', average(age));
+  console.log('standardDeviation(age)', standardDeviation(age));
 
   wstream.write(JSON.stringify(rows));
-  // wstream.write(JSON.stringify(rows));
-  // wstream.write(csvContent);
 });
 
 
